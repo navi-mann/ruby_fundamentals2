6b84d48af60e865d35449309d2cbd4028e811ef5
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-09 22:50:59 -0400

    prompted user temp. in Fahrenheit and defined method plus called method

--- exercise6.rb
+++ exercise6.rb
@@ -1,8 +1,18 @@
-put "What is the temmperature in Fahrenheit?"
-temmperature = gets.chomp.to_i
+# put "What is the temmperature in Fahrenheit?"
+# temmperature = gets.chomp.to_i
+#
+#
+# def fahrenheit(celsius)
+#   celsius = (temmperature - 32) * (5/9)
+# end
+#
+# fahrenheit (temmperature)
 
-def fahrenheit(celsius)
-  celsi
+puts "What is the temperature today in Fahrenheit?"
+temperature = gets.chomp.to_i
+
+def convert(fahrenheit)
+  return "foo"
 end
 
-fahrenheit (50)
+p "Today's temperature in celsius is #{convert (temperature)}."
